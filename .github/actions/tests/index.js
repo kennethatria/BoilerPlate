49a49ddd299a49fce7025df4dbf55241a977e360
kennethatria <kennethatria@gmail.com>
2022-05-09 14:11:19 +0200

    updates-with-extracts

--- .github/actions/tests/index.js
+++ .github/actions/tests/index.js
@@ -9,7 +9,57 @@ try {
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log(github.context.payload.issue.number);
+  user_ip = extractIP(github.context.payload.issue.body)
+  user_email = extractEmail(github.context.payload.issue.body)
+  console.log(user_email, user_ip);
+  console.log(github.context.payload.issue.body);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
 }
+
+function extractEmail(sample_body){
+    let email = 'Invalid or no email provided'
+    const text_body = text.match(/^(email: )(?<email>.*)$/m)
+    if (text_body?.groups?.email === undefined || text_body?.groups?.email === '') {
+      return false
+    } else {
+      email = text_body.groups.email.trim()
+    }
+    if(validateEmail(email)){
+        return email
+      }else{
+        return false
+      }
+}
+
+function extractIP(sample_body){
+    let ip = 'Invalid or no ip provided'
+    const text_body = text.match(/^(ip: )(?<ip>.*)$/m)
+    if (text_body?.groups?.ip === undefined || text_body?.groups?.ip === '') {
+      return false
+    } else {
+      ip = text_body.groups.ip.trim()
+    }
+    if(ValidateIPaddress(ip)){
+      return ip
+    }else{
+      return false
+    }
+}
+  
+  function ValidateIPaddress(ipaddress) {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {  
+      return true 
+    }
+  } 
+  
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+  
